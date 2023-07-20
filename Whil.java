261b92a6175919d365503dd6833823ba9715aad5
Krishnamurari7 <139735281+Krishnamurari7@users.noreply.github.com>
2023-07-20 20:07:10 +0530

    Add files via upload

---
+++ Whil.java
@@ -0,0 +1,10 @@
+public class Whil{
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=3){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("finishing while loop");
+    }
+}
---
+++ namewhile.java
@@ -0,0 +1,10 @@
+public class namewhile {
+    public static void main(String[] args) {
+        int n;
+        n = 1;
+        while(n<=10){
+            System.out.println("krishna"+n);
+            n++;
+        }
+    }
+}
