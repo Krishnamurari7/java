037637108edb8ad308c87c37545b0a0a8a66751a
Krishnamurari7 <139735281+Krishnamurari7@users.noreply.github.com>
2023-07-21 18:06:02 +0530

    Add files via upload

---
+++ str1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class str1 {
+    public static void main(String args[]){  
+        StringBuilder sb=new StringBuilder("Hello");  
+        sb.replace(1,4,"Java");  
+        System.out.println(sb);//prints HJavalo  
+        } 
+}
